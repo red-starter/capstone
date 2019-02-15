32b7557e930fc2e4afc9d417c7935572d019d221
Mike Sova <michalpso@gmail.com>
2019-02-15 15:49:54 -0600

    add sentiment analysis

---
+++ SentimentAnalysis.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+import json
+import pandas as pd
+from textblob import TextBlob
+import nltk
+from numpy.random import choice
+
+# in this file i compute the sentiment analyisis for all the tweets
+df = pd.read_csv('cleaned_tweets.csv')
+
+
+sentiments = {}
+from numpy.random import choice
+
+for date in df.columns:
+    text = df[date]
+    positive, negative, neutral = 0,0,0
+    for tweet in text:
+        analysis = TextBlob(str(tweet))
+        if analysis.sentiment[0]>0:
+           positive+=1
+        elif analysis.sentiment[0]<0:
+           negative+=1
+        else:
+           neutral+=1
+        sentiments[date] = [negative,neutral,positive]
+
+
+
+# sentiments
+# dump into json
+with open('sentiments_day_tweets.json', 'w') as fp:
+    json.dump(sentiments, fp)
+
+
+
+
